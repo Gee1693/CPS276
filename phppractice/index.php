70838382c3cb018291f2471e634fee34e25eb8ea
Gee1693 <94586747+Gee1693@users.noreply.github.com>
2022-10-17 18:06:30 -0400

    updates

--- phppractice/index.php
+++ phppractice/index.php
@@ -1,6 +1,106 @@
 <?php
 
-require_once "Calculator.php";
+$str = "Programming in PHP is cool";
+$arr = explode(" ",$str);
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+
+$arr = array("Programming","in","PHP","is","cool");
+$str = implode($arr); //BREAK ON EVERY COMMA AND CREATES ONE LONG STRING
+echo "$str<br />";
+$str = implode(" ",$arr); //BREAK ON EVERY COMMA AND INSERTS A SPACE
+echo "$str<br />";
+$str = implode("--",$arr); //BREAK ON EVERY COMMA AND INSERTS A "--"
+echo "$str<br />";
+
+
+// CODE FOR ASSISGNMENT 4
+
+<?php
+
+class addNames {
+
+    public function addClearNames() {
+
+        foreach($_POST as $value){
+            if($value === ""){
+
+                echo "";
+                //header('Location: index.php');
+                //exit;
+            }
+        }
+
+
+        if(isset($_POST['addName'])){
+            //hea   der("Location: index.php?fname={$_POST['fullName']}");
+            //$nmArr = explode(" ",$_POST['fullName']);
+
+            //
+
+            /*$nmArr = explode(" ",);
+            array_push($nmArr, $_POST['fullName']);
+            $name = $nmArr[1].", ".$nmArr[0];
+            //array_push($nmArr, $_POST['fullName']);
+            sort($nmArr);
+            $nmStr = implode(" ", $nmArr);*/
+
+            //
+
+            $nmArr = explode(" ",$_POST['fullName']);
+            //array_push($nmArr, $_POST['fullName']); // Array is wrong
+            $name = $nmArr[1].", ".$nmArr[0] . " \n";
+
+            $namelist = explode("\n", $_POST['nameList']);
+
+
+            array_push($namelist, $name);
+
+            sort($namelist);
+
+            $output = implode("\n", $namelist);
+
+            return $output;
+
+
+
+
+            //array_push($nmArr, $_POST['fullName']);
+            //sort($nmArr); // Array is wrong
+            //$nmStr = implode(" ", $nmArr);
+
+            //
+
+            //getType($name);
+
+            //$backSlash = explode(" ","/n");
+            //$newName = $nmArr[1]."/n".$nmArr[0];
+            //$name = $nmArr[1].", ".$nmArr[0];
+
+
+            //return $name;
+
+            //header("Location: ../acknowledgment3.php?fname={$_POST['fullName']}");
+        }
+        
+        else if(isset($_POST['clearNames'])){
+
+            return "";
+            //header("Location: index.php");
+        }
+
+    }
+
+}
+
+?>
+
+/*Hint for assignment 4: In a TEXT AREA section in form seperate the information in that section with \n instead of <br>.*/ 
+
+//Mr. Shaper's russet server examples https://russet-v8.wccnet.edu/~sshaper/cps276_examples/
+
+/*require_once "Calculator.php";
 $Calculator = new Calculator();
 echo $Calculator->calc("/", 10, 0); //will output Cannot divide by zero
 echo $Calculator->calc("*", 10, 2); //will output The product of the numbers is 20
@@ -8,7 +108,7 @@ echo $Calculator->calc("/", 10, 2); //will output The division of the numbers is
 echo $Calculator->calc("-", 10, 2); //will output The difference of the numbers is 8
 echo $Calculator->calc("+", 10, 2); //will output The sum of the numbers is 12
 echo $Calculator->calc("*", 10); //will output You must enter a string and two numbers
-echo $Calculator->calc(10); //will output You must enter a string and two numbers
+echo $Calculator->calc(10); //will output You must enter a string and two numbers*/
 
 /*$hello = 'Hello World';
 
---
+++ projects/project4/addNames.php
@@ -0,0 +1,44 @@
+<?php
+
+class addNames {
+
+    public function addClearNames() {
+
+        foreach($_POST as $value){
+            if($value === ""){
+
+                echo "";
+        
+            }
+        }
+
+
+        if(isset($_POST['addName'])){
+
+            $nmArr = explode(" ",$_POST['fullName']);
+           
+            $name = $nmArr[1].", ".$nmArr[0] . " \n";
+
+            $namelist = explode("\n", $_POST['nameList']);
+
+            array_push($namelist, $name);
+
+            sort($namelist);
+
+            $output = implode("\n", $namelist);
+
+            return $output;
+
+        }
+        
+        else if(isset($_POST['clearNames'])){
+
+            return "";
+           
+        }
+
+    }
+
+}
+
+?>
